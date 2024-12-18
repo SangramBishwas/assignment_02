ca61f1a48fbc6b495919013820bee5bd3465c388
Sangram Bishwas <info.sangram2003@gmail.com>
2024-12-18 12:54:38 +0600

    total revenue

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -43,6 +43,39 @@ const createOrder = async (req: Request, res: Response) => {
   }
 };
 
+const getRevenue = async (req: Request, res: Response) => {
+  try {
+    const result = await Order.aggregate([
+      {
+        $project: {
+          totalPrice: { $multiply: ['$price', '$quantity'] },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalRevenue: { $sum: '$totalPrice' },
+        },
+      },
+    ]);
+    const totalRevenue = result.length > 0 ? result[0].totalRevenue : 0;
+    res.status(200).json({
+      message: 'Revenue calculated successfully',
+      status: true,
+      data: {
+        totalRevenue: totalRevenue, 
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+      error,
+    });
+  }
+};
+
 export const orderController = {
   createOrder,
+  getRevenue,
 };
